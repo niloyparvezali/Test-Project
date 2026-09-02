bb78662092c044abab215aaa2038d136300c9d44
WhiteSauce <niloyparvez200@gmail.com>
2026-09-03 05:31:25 +0600

    Delete src/firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
-const required=['VITE_FIREBASE_API_KEY','VITE_FIREBASE_AUTH_DOMAIN','VITE_FIREBASE_PROJECT_ID','VITE_FIREBASE_MESSAGING_SENDER_ID','VITE_FIREBASE_APP_ID'];
-export const firebaseMissingConfig=required.filter(k=>!import.meta.env[k]);
-const app=initializeApp({apiKey:import.meta.env.VITE_FIREBASE_API_KEY,authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,projectId:import.meta.env.VITE_FIREBASE_PROJECT_ID,messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,appId:import.meta.env.VITE_FIREBASE_APP_ID});
-export const auth=getAuth(app);
-export const db=getFirestore(app);
